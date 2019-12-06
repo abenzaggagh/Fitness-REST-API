ca008f129ddfdf08a7fc16477a84192aac41a762
Amine BEN ZAGGAGH <33093327+4min3b3n@users.noreply.github.com>
2019-12-06 15:22:16 -0800

    Added Environment Variables

--- src/routers/habitRoutes.js
+++ src/routers/habitRoutes.js
@@ -3,14 +3,7 @@ import { addNewHabit } from "../controllers/habitController";
 const habitRoutes = (app) => {
 
     app.route('/habits')
-    // .get(getUsers)
-    // .get(getUser)
     .post(addNewHabit);
-
-    // app.route('/habit/:habitID')
-    // .get(getUser)
-    // .put(updateUser)
-    // .delete(deleteUser)
 
 }
 
